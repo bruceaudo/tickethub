c9eed3e2215030ae4833c522de02ab4ed38d6c88
bruceaudo <audo401@gmail.com>
2023-07-03 22:48:02 +0300

    Eighth commit

--- app/components/Feed.jsx
+++ app/components/Feed.jsx
@@ -14,7 +14,7 @@ const Feed = ()=> {
   useEffect(() => {
     const getData = async () => {
       setLoading(true)
-     const response = await fetch(`/api/all_products`);
+     const response = await fetch(`/api/all_products`)
      const data = await response.json();
       setProducts(data);
       setLoading(false)
@@ -23,41 +23,48 @@ const Feed = ()=> {
     getData();
   }, []);
     
-    return <>{products?.length === 0 && !loading && <div className="mt-[110px] md:mt-[120px] lg:mt-[130px] text-base flex items-center w-full">
-            <p className='min-w-[50%] mx-auto text-center'>No products have been added yet.</p>
-    </div>}
-      {products && !loading &&
-        <section className="flex flex-col min-h-screen justify-center items-center mt-[110px] md:mt-[120px] lg:mt-[130px]">
-          <div className="sm:my-6 my-10 grid lg:grid-cols-3 lg:gap-[60px] md:grid-cols-2 md:gap-10 grid-cols-1 sm:gap-y-0 gap-y-[62px]">
-            {products?.length > 0 && products.map((product) =>
-              <div
-                key={product._id}
-                className="rounded-lg shadow-md p-2 hover:scale-[1.05] transition-all cursor-pointer"
-              >
-                <Image
-                  width={300}
-                  height={500}
-                  src={product.imageURL}
-                  alt="Product_image"
-                  className="w-full sm:h-[400px] h-[500px] rounded-lg"
-                />
-                <p className="mt-4 font-semibold text-[18px] truncate">
-                  {product.name}
-                </p>
-                <span className="mt-4 block font-semibold">
-                  Ksh.{product.price}
-                </span>
-                <Link
-                  href={`/product?id=${product._id}`}
-                  className="w-full py-2 px-3 bg-[#0077be] text-white rounded-lg mt-4 block text-center shadow-lg"
-                >
-                  See more
-                </Link>
-              </div>
-            )}
+    return (
+      <>
+        {products?.length === 0 && !loading && (
+          <div className="mt-[110px] md:mt-[120px] lg:mt-[130px] text-base flex items-center w-full">
+            <p className="min-w-[50%] mx-auto text-center">
+              No products have been added yet.
+            </p>
           </div>
-        </section>}
-    {loading && <Loading />}
-    </>
+        )}
+        {products && !loading && (
+          <section className="flex flex-col min-h-screen justify-center items-center mt-[110px] md:mt-[120px] lg:mt-[130px] w-full">
+            <div className="sm:my-6 my-10 grid lg:grid-cols-3 lg:gap-[60px] md:grid-cols-2 md:gap-10 grid-cols-1 w-full sm:gap-y-0 gap-y-[62px]">
+              {products?.length > 0 &&
+                products.map((product) => (
+                  <Link
+                    href={`/product?id=${product._id}`}
+                    key={product._id}
+                    className="rounded-lg cursor-pointer p-1 block"
+                  >
+                    <Image
+                      width={300}
+                      height={500}
+                      src={product.imageURL}
+                      alt="Product_image"
+                      className="w-full sm:h-[400px] h-[500px] rounded-sm"
+                    />
+                    <p className="mt-4 font-semibold text-[18px] truncate">
+                      {product.name}
+                    </p>
+                    <span className="mt-4 block font-semibold">
+                      Ksh.{product.price}
+                    </span>
+                    <button className="w-full py-2 px-3 bg-gray-100 border-gray-200 text-black rounded-lg mt-4 mb-1.5 text-center font-semibold">
+                      See more
+                    </button>
+                  </Link>
+                ))}
+            </div>
+          </section>
+        )}
+        {loading && <Loading />}
+      </>
+    );
 }
 export default Feed;
--- app/components/MobileNav.jsx
+++ app/components/MobileNav.jsx
@@ -1,5 +1,6 @@
 "use client";
 import Link from "next/link";
+import LoginModal from './LoginModal';
 import React, { useEffect, useState } from "react";
 import {
   Bars3Icon,
@@ -35,67 +36,67 @@ export default function MobileNav({isOpen, setIsOpen}) {
     setUpProviders();
   }, []);
   return (
-    <div className="flex lg:hidden">
-      {!session?.user ? (
-        <div>
-          <button
-            className="shadow-none bg-transparent mr-5"
-            onClick={checkIfLoggedIn}
-          >
-            Sell
-          </button>
-          {providers &&
-            Object.values(providers).map((provider) => (
-              <button
-                type="button"
-                key={provider.name}
-                onClick={() => signIn(provider.id)}
-                className="rounded-lg px-3 w-fit py-2 bg-[#0077be] text-white text-sm"
-              >
-                Sign up / Sign in
-              </button>
-            ))}
-        </div>
-      ) : (
-        <div className="flex items-center">
-          <Link className="mr-5" href={"/sell/dashboard"}>
-            Sell
-          </Link>
-          <span
-            onClick={() => setIsOpen((prev) => !prev)}
-            className="flex gap-[2px] relative items-center cursor-pointer"
-          >
-            <Image
-              className="h-8 w-8 rounded-full"
-              width={32}
-              height={32}
-              src={session?.user.image}
-              alt="Profile"
-            />
-            <ChevronDownIcon className="w3 h-3" />
-            {isOpen && (
-              <div className="absolute shadow-lg bg-white border border-[#f4f4f4] w-[240px] top-[38px] rounded-lg px-5 py-5 right-[8px] z-50">
-                <h3 className="text-center text-base font-semibold text-black">
-                  What are you looking for?
-                </h3>
+      <div className="flex lg:hidden">
+        {!session?.user ? (
+          <div>
+            <button
+              className="shadow-none bg-transparent mr-5"
+              onClick={checkIfLoggedIn}
+            >
+              Sell
+            </button>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="rounded-lg px-3 w-fit py-2 bg-[#0077be] text-white text-sm"
+                >
+                  Sign up / Sign in
+                </button>
+              ))}
+          </div>
+        ) : (
+          <div className="flex items-center">
+            <Link className="mr-5" href={"/sell/dashboard"}>
+              Sell
+            </Link>
+            <span
+              onClick={() => setIsOpen((prev) => !prev)}
+              className="flex gap-[2px] relative items-center cursor-pointer"
+            >
+              <Image
+                className="h-8 w-8 rounded-full"
+                width={32}
+                height={32}
+                src={session?.user.image}
+                alt="Profile"
+              />
+              <ChevronDownIcon className="w3 h-3" />
+              {isOpen && (
+                <div className="absolute shadow-lg bg-white border border-[#f4f4f4] w-[240px] top-[38px] rounded-lg px-5 py-5 right-[8px] z-50">
+                  <h3 className="text-center text-base font-semibold text-black">
+                    What are you looking for?
+                  </h3>
 
-                <p
-                  onClick={() => signOut()}
-                  className="px-3 py-2 w-full mt-4 rounded-lg bg-[#0077be] text-white"
-                >
-                  Log out
-                </p>
-                <Link
-                  href={"/sell/dashboard"}
-                  className="px-3 inline-block py-2 w-full text-sm mt-5 border-t "
-                >
-                  Sell on Tickethub
-                </Link>
-              </div>
-            )}
-          </span>
-        </div>
-      )}
-    </div>
+                  <p
+                    onClick={() => signOut()}
+                    className="px-3 py-2 w-full mt-4 rounded-lg bg-[#0077be] text-white"
+                  >
+                    Log out
+                  </p>
+                  <Link
+                    href={"/sell/dashboard"}
+                    className="px-3 inline-block py-2 w-full text-sm mt-5 border-t "
+                  >
+                    Sell on Tickethub
+                  </Link>
+                </div>
+              )}
+            </span>
+          </div>
+        )}
+      </div>
   );
 }
